fe2cdbead92c1ba5e10cfba53f1e20db3cb9544b
ogaroh <ogaroerick1998@gmail.com>
2023-12-20 18:47:37 +0300

    fix: routes

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,25 +1,30 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class AppRoutes {
-//   static Route onGenerateRoutes(RouteSettings settings) {
-//     switch (settings.name) {
-//       case '/':
-//         return _materialRoute(const DailyNews());
+import '../../features/news/domain/entities/article.dart';
+import '../../features/news/presentation/pages/article_details/article_details.dart';
+import '../../features/news/presentation/pages/home/news.dart';
+import '../../features/news/presentation/pages/saved/saved_articles.dart';
 
-//       case '/ArticleDetails':
-//         return _materialRoute(
-//             ArticleDetailsView(article: settings.arguments as ArticleEntity));
+class AppRoutes {
+  static Route onGenerateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return _materialRoute(const NewsHome());
 
-//       case '/SavedArticles':
-//         return _materialRoute(const SavedArticles());
+      case '/article-details':
+        return _materialRoute(
+            ArticleDetailsView(article: settings.arguments as ArticleEntity));
 
-//       default:
-//         return _materialRoute(const DailyNews());
-//     }
-//   }
+      case '/saved-articles':
+        return _materialRoute(const SavedArticles());
 
-//   // custom route builder
-//   static Route<dynamic> _materialRoute(Widget view) {
-//     return MaterialPageRoute(builder: (_) => view);
-//   }
-// }
+      default:
+        return _materialRoute(const NewsHome());
+    }
+  }
+
+  // custom route builder
+  static Route<dynamic> _materialRoute(Widget view) {
+    return MaterialPageRoute(builder: (_) => view);
+  }
+}
