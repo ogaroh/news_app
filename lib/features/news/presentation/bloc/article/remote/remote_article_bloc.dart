c429cb609754fc10f565bfc7167264db2faea3f2
ogaroh <ogaroerick1998@gmail.com>
2023-12-20 18:35:49 +0300

    feat: remote article bloc

---
+++ lib/features/news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../../core/shared/data_state.dart';
+import '../../../../domain/usecases/get_remote_articles.dart';
+import 'remote_article_event.dart';
+import 'remote_article_state.dart';
+
+class RemoteArticlesBloc
+    extends Bloc<RemoteArticlesEvent, RemoteArticlesState> {
+  final GetArticleUseCase _getArticleUseCase;
+
+  RemoteArticlesBloc(this._getArticleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticlesState> emit) async {
+    final dataState = await _getArticleUseCase();
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+
+    if (dataState is DataFailed) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
