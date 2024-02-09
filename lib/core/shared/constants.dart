5e5301a37fd1e0c986a9e7ddba7602fbd4fe09c3
ogaroh <ogaroerick1998@gmail.com>
2024-02-09 14:34:10 +0300

    json key

--- lib/core/shared/constants.dart
+++ lib/core/shared/constants.dart
@@ -1,5 +1,5 @@
 
-const String kAPIKey = "db2b63624fa8426080baecb41f0112e9"; // TODO: use env
+const String kAPIKey = "db2b63624fa8426080baecb41f0112e9"; // TODO: use env / dart-define
 const String kBaseURL = 'https://newsapi.org/v2';
 const String kCountryQuery = 'us';
 const String kCategoryQuery = 'technology';
